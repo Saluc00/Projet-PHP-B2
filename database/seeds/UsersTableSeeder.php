007882640b7291eb01ab1a7698dc8b8ff4b87866
Mathieu <mathieu.caselles@ynov.com>
2019-10-22 17:23:26 +0200

    debug

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,19 +11,21 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 10)->create()->each(function ($user) {
+        factory(App\User::class, 3)->create()->each(function ($user) {
             $user->assignRole('admin');
             $user->profile()->save(factory(App\Profile::class)->make());
-        });   
-        
+        });
+
+        factory(App\User::class, 20)->create()->each(function ($user) {
+            $user->assignRole('vip');
+            $user->profile()->save(factory(App\Profile::class)->make());
+        });
+
         factory(App\User::class, 40)->create()->each(function ($user) {
             $user->assignRole('user');
             $user->profile()->save(factory(App\Profile::class)->make());
-        });  
+        });
 
-        factory(App\User::class, 50)->create()->each(function ($user) {
-            $user->assignRole('visiter');
-            $user->profile()->save(factory(App\Profile::class)->make());
-        });  
+        factory(App\User::class, 50)->create();
     }
 }
