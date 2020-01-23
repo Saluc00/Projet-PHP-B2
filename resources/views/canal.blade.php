0801c0393c8598f8369ac81852995cac59ae1487
Saluc00 <lucas.usereau@ynov.com>
2020-01-23 12:48:08 +0100

    CSS canal/message/profile

--- resources/views/canal.blade.php
+++ resources/views/canal.blade.php
@@ -2,60 +2,59 @@
 
 @section('content')
 
-    <div class="container">
-        </h1>Canal: <strong>{{ $canal->titre }}</strong></h1>
+<div class="container">
 
-        <div id="test" class="border p-3">
-            @foreach($messages as $message)
+    <h1 class="text-center m-3">Canal: <strong>{{ ucfirst($canal->titre) }}</strong></h1>
+
+    <div id="test"  style="height: 60vh;" class="border rounded bourder-dark p-3 m-t3 overflow-auto">
+        @foreach($messages as $message)
+            <div style="text-overflow: ellipsis; word-wrap: break-word;">
                 <p>
                     <a href="{{ url('profile').'/'.$message->user_id }}">{{ (DB::table('profiles')->where('user_id', '=', $message->user_id)->get())[0]->pseudo }}</a>: {{ $message->content }}
                 </p>
-            @endforeach
-        </div>
-
-        <br>
-
-        @if (! Auth::guest())
-
-            <form action="{{ url('canal').'/'.$canal->canal_id }}" method="post">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Écrivez
-                        votre message ici : </label>
-                    <textarea v-model="message" name="message" placeholder="Message" class="form-control"
-                              id="exampleFormControlTextarea1" rows="3"></textarea>
-                    <input type="submit" value="Envoyer" class="btn btn-primary mb-2 float-right">
-                </div>
-            </form>
-        @endif
-        <br>
-        <hr>
-        <a href="/canals">Retour au canals ici</a>
-
+            </div>
+        @endforeach
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/0.12.15/vue.min.js"></script>
-    <script>
-        var socket = io();
-        new Vue({
-            el: '#test',
+    @if (! Auth::guest())
+        <form action="{{ url('canal').'/'.$canal->canal_id }}" method="post" class="mt-3">
+            {{ csrf_field() }}
+            <div class="input-group">
+                <textarea name="message" class="form-control" aria-label="With textarea"></textarea>
+                <div class="input-group-prepend">
+                    <button class="btn btn-outline-primary" type="submit" id="button-addon1">Envoyer</button>
+                </div>
+            </div>
+        </form>
+    @endif
 
-            data: {
-                message: ''
-            },
+    <hr>
+<a href="/canals">Retour au canals ici</a>
 
-            methods: {
-                send: function (e) {
-                    socket.emit('message_send', this.message);
+</div>
 
-                    this.message = '';
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/0.12.15/vue.min.js"></script>
+<script>
+    var socket = io();
+    new Vue({
+        el: '#test',
 
-                    e.preventDefault();
-                }
+        data: {
+            message: ''
+        },
+
+        methods: {
+            send: function(e) {
+                socket.emit('message_send', this.message);
+
+                this.message = '';
+
+                e.preventDefault();
             }
+        }
 
-        })
-    </script>
+    })
+</script> --}}
 
 @endsection
--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -5,27 +5,25 @@
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js">console.log('oui')</script>
 
 <div class="container">
-    <div id="messages">
+    <div id="messages" style="height: 60vh;" class="border rounded bourder-dark p-3 m-3 overflow-auto">
         @foreach($messages as $message)
-        <li> {{ DB::table('profiles')->where('profile_id', '=', $message->profil_id)->get()[0]->pseudo }} ->
-            {{ $message->content }}</li>
+        <div style="text-overflow: ellipsis; word-wrap: break-word;">
+            <p><a href="/profile/{{ DB::table('profiles')->where('profile_id', '=', $message->profil_id)->get()[0]->profile_id }}">{{ DB::table('profiles')->where('profile_id', '=', $message->profil_id)->get()[0]->pseudo }}</a>:
+                {{ $message->content }}</p>
+        </div>
         @endforeach
     </div>
-<form action="/envoie/message/{{ $id }}-{{ $id2 }}" method="post">
 
+    <form action="/envoie/message/{{ $id }}-{{ $id2 }}" method="post" class="m-auto">
         {{ csrf_field() }}
-        <div>
-            <label class="label">Message</label>
-            <div>
-                <textarea name="message" class="form-control"></textarea>
+        <div class="input-group m-3">
+            <textarea name="message" class="form-control" aria-label="With textarea"></textarea>
+            <div class="input-group-prepend">
+                <button class="btn btn-outline-primary" type="submit" id="button-addon1">Envoyer</button>
             </div>
             @if($errors->has("message"))
             <p class="help is-danger">{{ $errors->first('message') }}</p>
             @endif
-        </div>
-
-        <div>
-            <button type=submit class="btn btn-primary">Publier</button>
         </div>
     </form>
 </div>
